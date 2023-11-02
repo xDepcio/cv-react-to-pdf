970adb3e0cf7fe2d75bdc6e88835327271e16782
xDepcio <olek.drwal@gmail.com>
2023-11-02 09:46:49 +0100

    added unoptimized flag

--- make-pdf.mjs
+++ make-pdf.mjs
@@ -3,6 +3,29 @@ global.btoa = (b) => Buffer.from(b).toString('base64')
 import fetch from 'node-fetch'
 import * as fs from 'fs'
 
+// fetch('https://api.pdfshift.io/v3/convert/pdf', {
+//     method: 'POST',
+//     headers: {
+//         Authorization: 'Basic ' + btoa('api:8ded585fd1b5414e92992970aa488f39'),
+//         'Content-type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//         // source: 'https://cv-react-to-pdf.vercel.app/cv',
+//         source: 'https://pl.wikipedia.org/wiki/Ewolucja_biologiczna',
+//         landscape: false,
+//         use_print: false,
+//         sandbox: true,
+//         format: `350mmx600mm`,
+//         delay: 2000,
+//         // javascript: `const ele = document.querySelector(\`body > div.h-screen.w-screen.overflow-x-hidden > div > section:nth-child(5) > div.mt-8.flex.flex-col.gap-16 > div.flex.gap-10.flex-row-reverse > img\`);ele.scrollIntoView();`
+//     })
+// }).then(response => {
+//     response.body.pipe(fs.createWriteStream('wiki.pdf'))
+// })
+
+// btoa and fetch are needed
+global.btoa = (b) => Buffer.from(b).toString('base64')
+
 fetch('https://api.pdfshift.io/v3/convert/pdf', {
     method: 'POST',
     headers: {
@@ -10,14 +33,13 @@ fetch('https://api.pdfshift.io/v3/convert/pdf', {
         'Content-type': 'application/json'
     },
     body: JSON.stringify({
+        // source: 'https://en.wikipedia.org/wiki/PDF',
         source: 'https://cv-react-to-pdf.vercel.app/cv',
         landscape: false,
         use_print: false,
         sandbox: true,
         format: `350mmx600mm`,
-        delay: 2000,
-        javascript: `const ele = document.querySelector(\`body > div.h-screen.w-screen.overflow-x-hidden > div > section:nth-child(5) > div.mt-8.flex.flex-col.gap-16 > div.flex.gap-10.flex-row-reverse > img\`);ele.scrollIntoView();`
     })
 }).then(response => {
-    response.body.pipe(fs.createWriteStream('cv.pdf'))
+    response.body.pipe(fs.createWriteStream('wikipedia.pdf'))
 })
